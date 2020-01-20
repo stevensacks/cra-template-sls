253b8bfb11b3f4af5f69c533c9c36c70584cf0b5
Steven Sacks <stevensacks@gmail.com>
2020-01-20 15:23:07 +0900

    initial push

---
+++ template/src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => (
+    <div className="app">
+        <header className="app-header">
+            <div>
+                <span>v0.1.0</span>
+            </div>
+        </header>
+    </div>
+);
+
+export default App;
---
+++ template/src/index.js
@@ -0,0 +1,9 @@
+import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import 'element-scroll-polyfill';
+import './index.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
